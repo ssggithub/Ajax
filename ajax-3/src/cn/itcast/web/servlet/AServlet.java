bc8d98859bf4758b40bc129f95c785b2609daa83
ssggithub <13691897490@163.com>
2017-02-26 16:23:59 +0800

    ajax-3   json的简单应用

---
+++ ajax-3/src/cn/itcast/web/servlet/AServlet.java
@@ -0,0 +1,20 @@
+package cn.itcast.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/*
+		 * 向客户端发送json串
+		 */
+		String str = "{\"name\":\"zhangSan\", \"age\":18, \"sex\":\"male\"}";
+		response.getWriter().print(str);
+		System.out.println(str);
+	}
+}
